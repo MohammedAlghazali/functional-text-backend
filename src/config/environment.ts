a4f23cdbaf434b4b29bf806454ed410b957a157b
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-05-01 01:25:20 +0300

    Fix typescript issues

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,7 +2,7 @@ const { NODE_ENV, DATABASE_URI, PORT } = process.env;
 
 const config = {
   database: {
-    uri: DATABASE_URI,
+    uri: DATABASE_URI || '',
   },
   port: PORT || 5000,
   nodeEnv: NODE_ENV || 'development',
