fcca5c45aa57d89afd76c3df90874b05fb3e7982
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-05-01 18:30:02 +0300

    remove the testing db changes

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,11 +1,8 @@
-const {
-  NODE_ENV, DATABASE_URI, PORT, TEST_DATABASE_URI,
-} = process.env;
+const { NODE_ENV, DATABASE_URI, PORT } = process.env;
 
 const config = {
   database: {
     uri: DATABASE_URI || '',
-    testUri: TEST_DATABASE_URI || '',
   },
   port: PORT || 5000,
   nodeEnv: NODE_ENV || 'development',
